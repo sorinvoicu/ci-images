925821901c53f1fdb29368ee11b2b8511a86e84f
sorinvoicu <61691256+sorinvoicu@users.noreply.github.com>
2024-04-05 10:34:05 +0000

    update extraction path

--- scripts/install_sys_deps.sh
+++ scripts/install_sys_deps.sh
@@ -17,4 +17,4 @@ apt-get autoclean -y
 rm -rf /var/lib/apt/lists/*
 
 # Install lychee URL checker
-curl -sL "https://github.com/lycheeverse/lychee/releases/download/v0.14.3/lychee-v0.14.3-x86_64-unknown-linux-gnu.tar.gz" | tar -xvzf - -C "$HOME/.local/bin"
+curl -sL "https://github.com/lycheeverse/lychee/releases/download/v0.14.3/lychee-v0.14.3-x86_64-unknown-linux-gnu.tar.gz" | tar -xvzf - -C "/usr/local/bin"
