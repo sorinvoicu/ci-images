7cab01eb77bd8798869e2bcb718a5b2f66e99c96
sorinvoicu <61691256+sorinvoicu@users.noreply.github.com>
2024-04-05 08:37:28 +0000

    update install script

--- scripts/install_sys_deps.sh
+++ scripts/install_sys_deps.sh
@@ -6,7 +6,6 @@ pkgs_to_install="\
 lbzip2 \
 rsync \
 qpdf \
-lychee \
 "
 apt-get update -y
 apt-get install -q -y ${pkgs_to_install}
@@ -15,3 +14,8 @@ apt-get install -q -y ${pkgs_to_install}
 apt-get autoremove -y
 apt-get autoclean -y
 rm -rf /var/lib/apt/lists/*
+
+# Install lychee URL checker
+curl -sL "https://github.com/lycheeverse/lychee/releases/download/v0.14.3/lychee-v0.14.3-x86_64-unknown-linux-gnu.tar.gz" | tar -xvzf - &&
+install -t "$HOME/.local/bin" -D lychee && 
+rm lychee
