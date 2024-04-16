f45c9e229a22e3a51f0b15349c1616b4afe985da
sorinvoicu <61691256+sorinvoicu@users.noreply.github.com>
2024-04-16 12:17:25 +0000

    remove tidyverse deps

--- scripts/install_sys_deps.sh
+++ scripts/install_sys_deps.sh
@@ -7,6 +7,7 @@ curl \
 lbzip2 \
 rsync \
 qpdf \
+wget \
 "
 apt-get update -y
 # shellcheck disable=SC2086
@@ -18,4 +19,4 @@ apt-get autoclean -y
 rm -rf /var/lib/apt/lists/*
 
 # Install lychee URL checker
-curl -sL "https://github.com/lycheeverse/lychee/releases/download/v0.14.3/lychee-v0.14.3-x86_64-unknown-linux-gnu.tar.gz" | tar -xvzf - -C "/usr/local/bin"
+curl -Ls https://github.com/lycheeverse/lychee/releases/download/v${LYCHEE_VERSION}/lychee-v${LYCHEE_VERSION}-$(arch)-unknown-linux-gnu.tar.gz | tar xz -C /usr/local/bin
